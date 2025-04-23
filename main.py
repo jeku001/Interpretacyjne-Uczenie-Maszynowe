38a15f46c0506776e47aee68f3d1bd5b6a886ae1
jekuP1 <jedslu@st.amu.edu.pl>
2025-04-23 21:10:11 +0200

    Fnish GAM

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import math
+from pygam import LinearGAM, s, f, l
+from sklearn.preprocessing import StandardScaler
 
-from analysis import Analysis
+data = pd.read_csv('data/raw_data/zal_hausing.csv')
+data = data.drop(columns=['zainter'])
+scaler = StandardScaler()
+scaled = scaler.fit_transform(data)
+X = scaled[:,:-1]
+y = scaled[:,-1]
 
-#data explanation
-analyzer = Analysis()
-data = analyzer.loadData('data/raw_data/zal_hausing.csv')
-print(data.head().to_string())
-analyzer.findMissingValues(data)
+
+gam = LinearGAM(s(0) + s(1) + s(2) + s(3) + s(4) + s(5) + s(6) + s(7))
+gam.fit(X,y)
